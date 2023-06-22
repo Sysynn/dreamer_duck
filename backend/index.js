1ca4611536f54c4e6bcedbc77647baf447b422c1
Synn <synn@Synnui-MacBookAir.local>
2023-06-22 14:21:21 +0900

    fix AWS lambda

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
-const apiKey = "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"
+const apiKey = "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"
+const serverless = require('serverless-http');
 const { Configuration, OpenAIApi } = require("openai");
 const express = require('express')
 var cors = require('cors')
@@ -10,11 +11,11 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 //CORS 이슈 해결
-// let corsOptions = {
-//     origin: 'https://www.domain.com',
-//     credentials: true
-// }
-app.use(cors());
+let corsOptions = {
+    origin: 'https://dreamer-duck.pages.dev',
+    credentials: true
+}
+app.use(cors(corsOptions));
 
 //POST 요청 받을 수 있게 만듬
 app.use(express.json()) // for parsing application/json
@@ -54,4 +55,6 @@ app.post('/dreamTell', async function (req, res) {
     res.json({"assistant": dream});
 });
 
-app.listen(3000)
+module.exports.handler = serverless(app);
+
+// app.listen(3000)
